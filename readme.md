4efc7009fb11968e52222b986238c278a2af596c
Moti Zharfati <the.research.100@gmail.com>
2023-05-19 11:38:57 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,20 @@
+## CONCURRENCY BUG
+
+### What I am trying to do?
+Create 2 functions:
+1. Step1 - Will trigger step2 10 times
+2. Step2 - Will run with concurrency of 2.
+
+### Expected Behaviour
+To see Step2 run with concurrency of 2.
+
+### Actual Behaviour
+Step2 run everything at once, both in local & production.
+
+### How to run?
+1. npm start
+2. Open http://127.0.0.1:8288
+3. Click "Send event" and set "name" to "step1-requested"
+4. You will send 10 missions working and all of them will finish in 3 seconds.
+
+Thanks
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const { Inngest } = require('inngest');
+const { serve } = require("inngest/express");
+
+process.env.INNGEST_EVENT_KEY = 'local';
+
+const app = express();
+const inngest = new Inngest({ name: "My App" });
+
+app.use(express.json());
+app.use(
+    "/api/inngest",
+    serve(inngest, [step1(), step2()])
+)
+
+function step1() {
+    return inngest.createFunction(
+        {name: `step1`},
+        {event: `step1-requested`},
+        async ({event, step}) => {
+            const missions = [];
+            for (let counter = 0; counter <= 10; counter++) {
+                missions.push({
+                    name: "step2-requested",
+                    data: {
+                        counter
+                    }
+                })
+            }
+            await step.sendEvent(missions);
+            return {name: "step1"};
+        }
+    );
+}
+
+function step2() {
+    return inngest.createFunction(
+        {name: `step2`, concurrency: 2},
+        {event: `step2-requested`},
+        async ({event, step}) => {
+            await step.sleep('3s');
+            console.log(`hello from counter: ${event.data.counter}`);
+            return { country: event.data.counter };
+        }
+    );
+}
+
+
+const PORT = 3002;
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
